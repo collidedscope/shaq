f4c8bd5330c3f419842edd2183a6664e4cd52503
Collided Scope <collidedscope+github@protonmail.com>
2021-05-02 20:51:40 -0400

    Extract import/export methods on Game

--- lib/shaq/game.cr
+++ lib/shaq/game.cr
@@ -1,6 +1,7 @@
+require "shaq/game/import_export"
+
 module Shaq
   class Game
-    START   = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
     PIECES  = {R: Rook, N: Knight, B: Bishop, Q: Queen, K: King}
     LETTERS = PIECES.to_h.invert
 
@@ -15,48 +16,6 @@ module Shaq
       tags = {} of String => String
 
     def initialize(@board, @turn, @castling, @ep_target, @hm_clock, @move)
-    end
-
-    def add_tag(key, value)
-      tags[key] = value
-    end
-
-    def self.from_fen(fen)
-      ranks, turn, castling, ep_target, hm_clock, move = fen.split
-
-      board = ranks
-        .delete('/')
-        .gsub(/(\d)/) { "." * $1.to_i }
-        .chars.map_with_index { |c, i|
-        Piece.from_letter(c).tap &.position = i if c != '.'
-      }
-      turn = {b: Side::Black, w: Side::White}[turn]
-
-      new board, turn, castling, ep_target, hm_clock.to_i, move.to_i
-    end
-
-    def self.from_pgn(pgn)
-      from_pgn_io IO::Memory.new pgn
-    end
-
-    def self.from_pgn_io(io)
-      new.tap do |game|
-        while line = io.gets
-          case line
-          when /\[(\S+) "(.+?)"\]/
-            game.add_tag $1, $2
-          else
-            line.scan /\d+\. (\S+) (\S+)/ do |(_, white, black)|
-              game.ply white
-              game.ply black unless black[/\d-/]?
-            end
-          end
-        end
-      end
-    end
-
-    def self.from_pgn_file(path)
-      File.open(path) { |f| from_pgn_io f }
     end
 
     def self.new
---
+++ lib/shaq/game/import_export.cr
@@ -0,0 +1,47 @@
+module Shaq
+  class Game
+    START = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+
+    def self.from_fen(fen)
+      ranks, turn, castling, ep_target, hm_clock, move = fen.split
+
+      board = ranks
+        .delete('/')
+        .gsub(/(\d)/) { "." * $1.to_i }
+        .chars.map_with_index { |c, i|
+        Piece.from_letter(c).tap &.position = i if c != '.'
+      }
+      turn = {b: Side::Black, w: Side::White}[turn]
+
+      new board, turn, castling, ep_target, hm_clock.to_i, move.to_i
+    end
+
+    def self.from_pgn(pgn)
+      from_pgn_io IO::Memory.new pgn
+    end
+
+    def self.from_pgn_io(io)
+      new.tap do |game|
+        while line = io.gets
+          case line
+          when /\[(\S+) "(.+?)"\]/
+            game.add_tag $1, $2
+          else
+            line.scan /\d+\. (\S+) (\S+)/ do |(_, white, black)|
+              game.ply white
+              game.ply black unless black[/\d-/]?
+            end
+          end
+        end
+      end
+    end
+
+    def self.from_pgn_file(path)
+      File.open(path) { |f| from_pgn_io f }
+    end
+
+    def add_tag(key, value)
+      tags[key] = value
+    end
+  end
+end
