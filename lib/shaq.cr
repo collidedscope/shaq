0a2d3cc510a4467e642f0483d4f5c403321d0ce2
Collided Scope <collidedscope+github@protonmail.com>
2021-05-03 19:51:05 -0400

    Add Shaq.load_pgn for ingesting multiple games from a single file

--- lib/shaq.cr
+++ lib/shaq.cr
@@ -5,6 +5,18 @@ module Shaq
 
   BACK_RANKS = {Side::White => 1, Side::Black => 8}
   PAWN_RANKS = {Side::White => 2, Side::Black => 7}
+
+  def self.load_pgn(path)
+    games = [] of Game
+
+    File.open path do |io|
+      while game = Game.from_pgn_io io
+        games << game
+      end
+    end
+
+    games
+  end
 end
 
 require "shaq/util"
