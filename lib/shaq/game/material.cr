34274a58fb487d6108dbe91b238633b2a326db14
Collided Scope <collidedscope+github@protonmail.com>
2021-05-05 03:10:50 -0400

    Add Game#material_imbalance

--- lib/shaq/game/material.cr
+++ lib/shaq/game/material.cr
@@ -1,4 +1,7 @@
 class Shaq::Game
+  alias PieceType = (Queen | Rook | Bishop | Knight | Pawn).class
+  alias Material = Hash(PieceType, Int32)
+
   def material(side)
     pieces.select(&.side.== side).reject &.king?
   end
@@ -16,5 +19,18 @@ class Shaq::Game
 
   def material_value
     material.transform_values { |tally| material_value tally }
+  end
+
+  def material_imbalance
+    black, white = material.values
+    imbalance = {Side::Black => Material.new, Side::White => Material.new}
+
+    VALUES.each_key do |piece|
+      diff = black.fetch(piece, 0) - white.fetch(piece, 0)
+      imbalance[Side::Black][piece] = diff if diff > 0
+      imbalance[Side::White][piece] = -diff if diff < 0
+    end
+
+    imbalance
   end
 end
