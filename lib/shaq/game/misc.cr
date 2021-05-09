1447e77d1665640999804fed0b08823d17614988
Collided Scope <collidedscope+github@protonmail.com>
2021-05-09 10:19:01 -0400

    Add Game#opening to miscellaneous methods

--- lib/shaq/game/misc.cr
+++ lib/shaq/game/misc.cr
@@ -2,4 +2,17 @@ class Shaq::Game
   def occupancy
     board.each_with_index.sum { |e, i| (e ? 1u64 : 0u64) << i }
   end
+
+  def opening
+    tree = Shaq.opening_tree
+    last_known = leaf = nil
+
+    san_history.each do |move|
+      last_known = leaf if leaf = tree[nil]?
+      break unless trimmed = tree[move]?
+      tree = trimmed
+    end
+
+    tree[nil]? || last_known
+  end
 end
