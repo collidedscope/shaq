5c7f94d007199dacea302c2bb5a4522dc112003d
Collided Scope <collidedscope+github@protonmail.com>
2021-05-11 06:28:45 -0400

    Add mate-in-one finding as a spec
    This is a somewhat lazy but fairly comprehensive "integration" test that
    at least gestures in the direction of convincing us that Shaq understands
    how all the pieces move.

--- spec/shaq_spec.cr
+++ spec/shaq_spec.cr
@@ -1,3 +1,4 @@
+require "csv"
 require "./spec_helper"
 
 describe Shaq do
@@ -7,6 +8,20 @@ describe Shaq do
       pieces.size.should eq 32
       turn.should eq Side::White
       legal_moves.size.should eq 20
+    end
+  end
+
+  it "can solve 'hard' mate-in-one puzzles" do
+    File.open "#{__DIR__}/fixtures/mate_in_one.csv" do |io|
+      CSV.new io, headers: true do |puzzle|
+        setup, solution = puzzle["moves"].split
+
+        subject Game.from_fen puzzle["FEN"] do
+          ply setup
+          mates = legal_moves.select { |move| sim(*move).ply.checkmate? }
+          mates.map { |m| Util.to_uci *m }.should contain solution
+        end
+      end
     end
   end
 end
