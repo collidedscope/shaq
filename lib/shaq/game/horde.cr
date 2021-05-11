53fab5dffad9d53d31219b821bd509855aacb00a
Collided Scope <collidedscope+github@protonmail.com>
2021-05-11 17:46:58 -0400

    Add Variant tag in HordeGame constructor

--- lib/shaq/game/horde.cr
+++ lib/shaq/game/horde.cr
@@ -1,7 +1,8 @@
 module Shaq
   class HordeGame < Game
     def self.new
-      from_fen "rnbqkbnr/pppppppp/8/1PP2PP1#{"/PPPPPPPP" * 4} w kq - 0 1"
+      game = from_fen "rnbqkbnr/pppppppp/8/1PP2PP1#{"/PPPPPPPP" * 4} w kq - 0 1"
+      game.tap &.add_tag "Variant", "Horde"
     end
 
     def check?
