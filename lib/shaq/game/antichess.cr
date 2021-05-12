f2eed3e8c7d29ab16a20051465dfaac91b648733
Collided Scope <collidedscope+github@protonmail.com>
2021-05-12 04:17:15 -0400

    Begin adding support for Antichess variant

---
+++ lib/shaq/game/antichess.cr
@@ -0,0 +1,32 @@
+module Shaq
+  class AntichessGame < Game
+    def self.new
+      game = from_fen STANDARD
+      game.tap &.add_tag "Variant", "Antichess"
+    end
+
+    def check?
+      false
+    end
+
+    def checkmate?
+      false
+    end
+
+    def can_capture?
+      !(vision & enemies.map &.position).empty?
+    end
+
+    def legal_moves_for(piece) : Array(Int32)
+      moves = piece.moves self
+      moves.select! &->occupied?(Int32) if can_capture?
+      moves
+    end
+  end
+
+  class Game
+    def antichess?
+      is_a? AntichessGame
+    end
+  end
+end
