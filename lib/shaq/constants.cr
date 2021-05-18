99079b32b17fc6dc166be04ce09a5518cee00e3d
Collided Scope <collidedscope+github@protonmail.com>
2021-05-18 16:30:39 -0400

    Give the King a value for symmetry's sake

--- lib/shaq/constants.cr
+++ lib/shaq/constants.cr
@@ -1,7 +1,7 @@
 module Shaq
   STANDARD = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
-  VALUES   = {Queen: 9, Rook: 5, Bishop: 3, Knight: 3, Pawn: 1}
   PIECES   = {K: King, Q: Queen, R: Rook, B: Bishop, N: Knight, P: Pawn}
+  VALUES   = {King: 0, Queen: 9, Rook: 5, Bishop: 3, Knight: 3, Pawn: 1}
   LETTERS  = PIECES.to_h.invert
   ROYAL    = {-9, -8, -7, -1, 1, 7, 8, 9}
   UCI      = /^([a-h][1-8]){2}[qnrb]?/
