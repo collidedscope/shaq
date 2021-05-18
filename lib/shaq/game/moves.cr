fa22c19e767e3470827fe9c22c6723fdb3725eed
Collided Scope <collidedscope+github@protonmail.com>
2021-05-18 07:19:54 -0400

    Add explicit return type to `Game#legal_moves_for`
    This partially reverts 76ae51811a3235ece5f5ddb8ab000b96ba14d854.
    
    The compiler can usually infer the return type of the block in
    `Game#legal_moves`, but fails to do so if the Game was parsed
    (`.from_pgn_io`) rather than explicitly constructed (`.from_fen`).
    
    Probably worth investigating further, but should be fine for now.

--- lib/shaq/game/moves.cr
+++ lib/shaq/game/moves.cr
@@ -10,7 +10,7 @@ class Shaq::Game
     clone.ply *args
   end
 
-  def legal_moves_for(piece)
+  def legal_moves_for(piece) : Array(Int32)
     piece.moves(self).reject { |square| sim(piece, square).check? }
   end
 
