f217d799c6c1ec952b3c633210c9640f507af386
Collided Scope <collidedscope+github@protonmail.com>
2021-06-02 01:02:28 -0400

    Refactor `CloakDaggerGame#ply` to use cloaked pieces
    Instead of trying to shoehorn the necessary logic into the legal move
    generation of the game itself (`CloakDaggerGame#legal_moves_for`), we
    instead override `#ply` to cloak pieces as and when they're captured.

--- lib/shaq/game/variants/cloak_and_dagger.cr
+++ lib/shaq/game/variants/cloak_and_dagger.cr
@@ -4,7 +4,15 @@ module Shaq
 
     def ply(from : Int32, to)
       super
-      cloaks[other_side] = (piece = capture) && piece.turncloak! if real?
+
+      if real? && (e = enemies.find &.cloak)
+        board[e.position] = e.disrobe
+      end
+
+      if piece = capture
+        board[to] = board[to].not_nil!.encloak piece
+      end
+
       self
     end
   end
