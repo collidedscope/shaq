b8f5cd8d0d3366ed784dd1cd9ad32614be1ca8aa
Collided Scope <collidedscope+github@protonmail.com>
2021-06-17 07:33:11 -0400

    Simplify losing condition determination for three-check variant

--- lib/shaq/game/variants/three_check.cr
+++ lib/shaq/game/variants/three_check.cr
@@ -2,14 +2,10 @@ module Shaq
   class ThreeCheckGame < Game
     variant "Three-check"
 
-    property checks = {Side::Black => 0, Side::White => 0}
-
-    def update
-      super.tap { checks[turn] += 1 if check? }
-    end
-
     def lost?
-      super || checks[turn] == 3
+      super || san_history.each_with_index.count { |move, i|
+        i & 1 == turn.to_i && move[-1] == '+'
+      } > 2
     end
   end
 end
