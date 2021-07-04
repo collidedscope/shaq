7445c71371a2f3b4b3526634f37da1424fdd16b4
Collided Scope <collidedscope+github@protonmail.com>
2021-07-04 04:31:44 -0400

    Add losing condition spec for `HordeGame`

--- spec/horde_spec.cr
+++ spec/horde_spec.cr
@@ -8,4 +8,23 @@ describe HordeGame do
       pieces(Side::White).all? &.pawn?
     end
   end
+
+  it "knows White has lost when their last piece is taken" do
+    subject HordeGame.from_diagram <<-EOD do
+      b - - 0 1
+      8 . . . . . . . .
+      7 . . . . . . . .
+      6 . . . b . . . .
+      5 . . . . . . . .
+      4 . . . . . . . Q
+      3 . . . . . n . .
+      2 . . . . . . . .
+      1 . . . . . . . k
+        a b c d e f g h
+      EOD
+      ply F3, H4
+      lost?.should be_true
+      san_history.last.should eq "Nxh4#"
+    end
+  end
 end
